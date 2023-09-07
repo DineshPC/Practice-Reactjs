7606bd42b6db8127f3d6274168610d1fe39db863
Dinesh <dineshcoudhary38@gmail.com>
2023-09-07 22:05:17 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./footerStyle.css"
+
+export default function Footer() {
+  return (
+    <div className='footerContainer'>
+        <h4>Footer section</h4>
+    </div>
+  )
+}
